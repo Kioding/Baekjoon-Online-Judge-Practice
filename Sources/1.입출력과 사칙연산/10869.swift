a93c8a44606dfcde3cf43b17d4527eaf743ae5f5
Kio <65153742+Kioding@users.noreply.github.com>
2021-04-22 03:46:39 +0900

    Create 10869.swift

---
+++ Sources/1.입출력과 사칙연산/10869.swift
@@ -0,0 +1,22 @@
+// '21.04.22
+import Foundation
+
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+print("a + b = \(a + b)")
+print("a - b = \(a - b)")
+print("a * b = \(a * b)")
+print("a / b = \(a / b)")
+print("a % b = \(a % b)")
+
+
+// 2
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+print("""
+        a + b = \(a + b)
+        a - b = \(a - b)
+        a * b = \(a * b)
+        a / b = \(a / b)
+        a % b = \(a % b)
+        """)
