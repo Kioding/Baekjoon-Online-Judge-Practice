397f455485fdbfc9c794662ce6fea84687b23b03
kio <un19451024@gmail.com>
2021-04-22 02:50:38 +0900

    [Feat] BOJ 1000 swift

---
+++ Sources/01XXX/01XXX/1000/main.swift
@@ -0,0 +1,12 @@
+//
+//  1000.swift
+//  01XXX
+//
+//  Created by kio on 2021/04/22.
+//
+
+import Foundation
+
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+print("a + b = \(a + b)")
--- Sources/01XXX/1000/README.md
+++
@@ -1,5 +0,0 @@
-[BOJ 1000 문제](https://www.acmicpc.net/problem/1000)
-
-- 문제: A+B   
-- 입력: 첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)   
-- 출력: 첫째 줄에 A+B를 출력한다.
