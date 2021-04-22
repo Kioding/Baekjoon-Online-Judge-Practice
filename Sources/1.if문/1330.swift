95aae04896f3d72d4a1162b362275cb80e788b11
Kio <65153742+Kioding@users.noreply.github.com>
2021-04-22 13:35:45 +0900

    Create 1330.swift

---
+++ Sources/1.if문/1330.swift
@@ -0,0 +1,13 @@
+// '21.04.22
+import Foundation
+
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+
+if a > b {
+    print(">")
+} else if a < b {
+    print("<")
+} else {
+    print("==")
+}
