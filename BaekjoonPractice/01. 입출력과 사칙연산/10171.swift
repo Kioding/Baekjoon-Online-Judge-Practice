9d5713e7bf3e06ac913e760d75c9713d958a5204
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-01 22:07:19 +0900

    [BOJ] 고양이

---
+++ BaekjoonPractice/01. 입출력과 사칙연산/10171.swift
@@ -0,0 +1,23 @@
+//
+//  main.swift
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/01.
+//
+
+import Foundation
+
+// 풀이1
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \\(__)|")
+
+// 풀이2
+print("""
+\\    /\\
+ )  ( ')
+(  /  )
+ \\(__)|
+""")
+
