cad53d32bcfdd5927ccec6cbac50fd31adca834a
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-01 22:16:00 +0900

    [BOJ] 개

---
+++ BaekjoonPractice/01. 입출력과 사칙연산/10172.swift
@@ -0,0 +1,35 @@
+//
+//  main.swift
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/01.
+//
+
+import Foundation
+
+// 풀이1
+print("|\\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\__|")
+
+// 풀이2
+print("""
+|\\_/|
+|q p|   /}
+( 0 )\"\"\"\\
+|\"^\"`    |
+||_/=\\__|
+""")
+
+// 풀이3
+let dog = #"""
+|\_/|
+|q p|   /}
+( 0 )"""\
+|"^"`    |
+||_/=\\__|
+"""#
+print(dog)
+
