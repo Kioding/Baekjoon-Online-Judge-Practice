4c71a72b77d14b09a8a3eff448b26e473d37de21
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-01 21:55:15 +0900

    [BOJ] We love kriii

---
+++ BaekjoonPractice/01. 입출력과 사칙연산/10718.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/01.
+//
+
+import Foundation
+
+// 풀이1
+print("강한친구 대한공군")
+print("강한친구 대한공군")
+
+// 풀이2
+print("강한친구 대한공군 \n강한친구 대한공군")
+
+// 풀이3
+let encouragementMessage = "강한친구 대한공군"
+for _ in 1...2 {
+    print(encouragementMessage)
+}
+
