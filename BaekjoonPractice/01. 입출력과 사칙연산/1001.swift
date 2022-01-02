7385ab04c2471689968746cb9c0b2415facfa25d
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-02 21:16:38 +0900

    [BOJ] A-B

---
+++ BaekjoonPractice/01. 입출력과 사칙연산/1001.swift
@@ -0,0 +1,18 @@
+//
+//  main.swift
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/01.
+//
+
+import Foundation
+
+// 풀이1
+let a = Int(readLine()!)!
+let b = Int(readLine()!)!
+print(a-b)
+
+// 풀이2
+let userInput = readLine()!
+let array = userInput.components(separatedBy: " ")
+print(Int(array[0])! - Int(array[1])!)
