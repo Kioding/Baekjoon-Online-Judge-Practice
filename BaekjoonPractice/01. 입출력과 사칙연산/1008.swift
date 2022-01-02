84e2eb9bd3d7106c9900429590378e7980f4683d
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-03 07:11:45 +0900

    [BOJ] A/B

---
+++ BaekjoonPractice/01. 입출력과 사칙연산/1008.swift
@@ -0,0 +1,29 @@
+//
+//  main.swift
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/03.
+//
+
+import Foundation
+
+// 풀이1
+let userInput = readLine()!
+let array = userInput.split(separator: " ")
+let a = Double(array[0])!
+let b = Double(array[1])!
+print(a / b)
+
+// 풀이2
+let userInput2 = readLine()!
+let array2 = userInput2.components(separatedBy: " ")
+let c = Double(array2[0])!
+let d = Double(array2[1])!
+print(c / d)
+
+// 풀이3
+let userInput3 = readLine()!
+let array3 = userInput2.components(separatedBy: " ").map { Double($0)! }
+let e = array3[0]
+let f = array3[1]
+print(e / f)
