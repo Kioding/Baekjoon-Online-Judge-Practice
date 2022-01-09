ff8b5e0e7fd53ba8e1ce46189b262c55d250f879
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-09 23:47:30 +0900

    [BOJ] 구구단

---
+++ BaekjoonPractice/03. for문/2739.swift
@@ -0,0 +1,15 @@
+//
+//
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/09.
+//
+
+import Foundation
+
+// 입력조건: 1 <= userInput <= 9
+let userInput = Int(readLine()!)!
+
+for index in 1...9 {
+    print("\(userInput) * \(index) = \(userInput*index)")
+}
