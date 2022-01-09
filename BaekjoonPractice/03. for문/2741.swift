fcb2395d60a9e085df5bca8358b5b8b662dcf0cc
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-10 00:26:10 +0900

    [BOJ] N 찍기

---
+++ BaekjoonPractice/03. for문/2741.swift
@@ -0,0 +1,15 @@
+//
+//
+//  BaekjoonPractice
+//
+//  Created by kio on 2022/01/10.
+//
+
+import Foundation
+
+// 자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+let userInput = Int(readLine()!)!
+
+for index in 1...userInput {
+    print(index)
+}
